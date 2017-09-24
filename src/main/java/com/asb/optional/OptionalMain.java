896883e8edae3d948bcb6e62b14203587c253e37
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-09-24 12:33:20 +0530

    Pipeline

--- src/main/java/com/asb/optional/OptionalMain.java
+++ src/main/java/com/asb/optional/OptionalMain.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 public class OptionalMain {
     public static void main(String[] args) {
         Optional<Integer> numOptional = Optional.of(5)
-                .map(num -> num + 5);
+                .map(num -> num + 5)
+                .map(num -> num * 2)
+                .map(num -> num - 2);
         System.out.println(numOptional);
     }
 }
