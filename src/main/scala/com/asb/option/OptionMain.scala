e58efbcf84bff972d145a66ab269ea8946e2050f
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-09-24 13:11:54 +0530

    More For Comprehensions

--- src/main/scala/com/asb/option/OptionMain.scala
+++ src/main/scala/com/asb/option/OptionMain.scala
@@ -2,19 +2,20 @@ package com.asb.option
 
 object OptionMain {
 
-  def sum(a: Int, b: Int): Int =
-    a + b
-
   def main(args: Array[String]): Unit = {
     val a = Some(5)
     val b = Some(6)
+    val c = Some(7)
+    val d = Some(8)
 
     // val res = a.flatMap(num1 => b.map(num2 => num1 + num2))
 
     val res = for {
       num1 <- a
       num2 <- b
-    } yield num1 + num2
+      num3 <- c
+      num4 <- d
+    } yield num1 + num2 + num3 + num4
 
     println(res)
   }
