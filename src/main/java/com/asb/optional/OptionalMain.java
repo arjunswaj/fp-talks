45b7bb55f21474f0563eebe867b2c248393ea6b7
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-09-24 12:49:25 +0530

    Sqrt

--- src/main/java/com/asb/optional/OptionalMain.java
+++ src/main/java/com/asb/optional/OptionalMain.java
@@ -3,21 +3,16 @@ package com.asb.optional;
 import java.util.Optional;
 
 public class OptionalMain {
-    public static void main(String[] args) {
-        Optional<Integer> numOptional = Optional.of(5)
-                .map(num -> num + 5)
-                .map(num -> num * 2)
-                .map(num -> num - 2)
-                .filter(num -> num % 2 == 0)
-                .map(num -> num * 5);
-        System.out.println(numOptional);
 
-        Optional<Integer> numOptional2 = Optional.of(5)
-                .map(num -> num + 5)
-                .map(num -> num * 2)
-                .map(num -> num - 2)
-                .filter(num -> num % 2 != 0)
-                .map(num -> num * 5);
-        System.out.println(numOptional2);
+    public static Optional<Double> sqrt(int num) {
+        if (num < 0) {
+            return Optional.empty();
+        } else {
+            return Optional.of(Math.sqrt(num));
+        }
+    }
+
+    public static void main(String[] args) {
+        Optional<Integer> numOptional = Optional.of(5);
     }
 }
--- src/main/scala/com/asb/option/OptionMain.scala
+++ src/main/scala/com/asb/option/OptionMain.scala
@@ -1,22 +1,13 @@
 package com.asb.option
 
 object OptionMain {
+
+  def sqrt(num: Int): Option[Double] =
+    if (num < 0) None
+    else Some(Math.sqrt(num))
+
   def main(args: Array[String]): Unit = {
     val numOption = Some(5)
-      .map(num => num + 5)
-      .map(num => num * 2)
-      .map(num => num - 2)
-      .filter(num => num % 2 == 0)
-      .map(num => num * 5)
-    println(numOption)
-
-    val numOption2 = Some(5)
-      .map(num => num + 5)
-      .map(num => num * 2)
-      .map(num => num - 2)
-      .filter(num => num % 2 != 0)
-      .map(num => num * 5)
-    println(numOption2)
-
+    val sqrtNum = ???
   }
 }
