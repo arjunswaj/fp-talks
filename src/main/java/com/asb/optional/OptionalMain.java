e8060a250ea4519f662aeae9051a9ff1425ba4f2
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-09-24 12:29:11 +0530

    Adding Optional Main class

---
+++ src/main/java/com/asb/optional/OptionalMain.java
@@ -0,0 +1,10 @@
+package com.asb.optional;
+
+import java.util.Optional;
+
+public class OptionalMain {
+    public static void main(String[] args) {
+        Optional<Integer> num = Optional.of(5);
+        System.out.println(num);
+    }
+}
