d8486787999b7bdf43be79bea3dc05d812b7909b
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-09-24 13:18:52 +0530

    Adding filter

--- src/main/scala/com/asb/option/OptionMain.scala
+++ src/main/scala/com/asb/option/OptionMain.scala
@@ -8,11 +8,15 @@ object OptionMain {
     val c = Some(7)
     val d = Some(8)
 
-    // val res = a.flatMap(num1 => b.flatMap(num2 => c.flatMap(num3 => d.map(num4 => num1 + num2 + num3 + num4))))
+    val res2 = a.flatMap(num1 => b
+      .filter(num2 => num1 + num2 > 66)
+      .flatMap(num2 => c.flatMap(num3 => d.map(num4 => num1 + num2 + num3 + num4))))
+
+    println(res2)
 
     val res = for {
       num1 <- a
-      num2 <- b
+      num2 <- b if (num1 + num2) > 66
       num3 <- c
       num4 <- d
     } yield num1 + num2 + num3 + num4
