2ea1f89bdfdff1b3d7f58fe382a951b519342dac
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-09-24 13:09:32 +0530

    For comprehension

--- src/main/scala/com/asb/option/OptionMain.scala
+++ src/main/scala/com/asb/option/OptionMain.scala
@@ -9,7 +9,13 @@ object OptionMain {
     val a = Some(5)
     val b = Some(6)
 
-    val res = a.flatMap(num1 => b.map(num2 => num1 + num2))
+    // val res = a.flatMap(num1 => b.map(num2 => num1 + num2))
+
+    val res = for {
+      num1 <- a
+      num2 <- b
+    } yield num1 + num2
+
     println(res)
   }
 }
