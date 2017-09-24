7cc891c9417768e8446f878eab10910d84fa56a9
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-09-24 14:39:10 +0530

    Fetch PIN for a person

--- src/main/scala/com/asb/option/OptionMain.scala
+++ src/main/scala/com/asb/option/OptionMain.scala
@@ -2,25 +2,33 @@ package com.asb.option
 
 object OptionMain {
 
+  case class Employee(id: Long, name: String, age: Long, team: String)
+
+  case class Address(id: Long, empID: Long, line1: String, line2: String, city: String, state: String, pin: String)
+
+  def getEmployeeIdByEmployeeName(name: String): Option[Long] = name match {
+    case "Arjun" => Some(1L)
+    case "Aditya" => Some(2L)
+    case _ => None
+  }
+
+  def getEmployeeById(id: Long): Option[Employee] = id match {
+    case 1L => Some(Employee(1L, "Arjun", 28, "SSR"))
+    case 2L => Some(Employee(2L, "Aditya", 23, "SE"))
+    case _ => None
+  }
+
+  def getAddressByEmpId(empId: Long): Option[Address] = empId match {
+    case 1L => Some(Address(1L, 1L, "KGS Lyt", "Vijayanagar", "BLR", "KAR", "560040"))
+    case 2L => Some(Address(2L, 2L, "Bagmane", "Mahadevpura", "BLR", "KAR", "560079"))
+    case _ => None
+  }
+
   def main(args: Array[String]): Unit = {
-    val a = Some(5)
-    val b = Some(6)
-    val c = Some(7)
-    val d = Some(8)
-
-    val res2 = a.flatMap(num1 => b
-      .filter(num2 => num1 + num2 > 66)
-      .flatMap(num2 => c.flatMap(num3 => d.map(num4 => num1 + num2 + num3 + num4))))
-
-    println(res2)
-
-    val res = for {
-      num1 <- a
-      num2 <- b if (num1 + num2) > 66
-      num3 <- c
-      num4 <- d
-    } yield num1 + num2 + num3 + num4
-
-    println(res)
+    val pin = getEmployeeIdByEmployeeName("Arjun")
+      .flatMap(id => getEmployeeById(id)
+        .flatMap(emp => getAddressByEmpId(emp.id)))
+      .map(address => address.pin)
+    println(pin)
   }
 }
